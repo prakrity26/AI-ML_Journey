03c9b3f744fc1569e38f87628026845a55b8e7dc
Prakriti Khanal <prakritykhanal1@gmail.com>
2026-06-14 21:13:08 +0545

    Learned dictionary

---
+++ Day_02/dictionary.py
@@ -0,0 +1,10 @@
+student = {'name':'John', 'age': '21', 'courses': ['maths','english']}
+
+
+# print(student)
+# print(student['name'])
+# print(student['courses'])
+
+for key, value in student.items():
+    print(key,value)
+
