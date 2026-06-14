bc258b7628fb9a997c6b317393cdc903e87bea6e
Prakriti Khanal <prakritykhanal1@gmail.com>
2026-06-14 21:09:46 +0545

    Implemented Lists

---
+++ Day_02/lists.py
@@ -0,0 +1,18 @@
+courses = ['maths','english','nepali','science']
+
+# nums =[1,5,6,3,8]
+
+# nums.sort()
+# for courses in courses:
+#     print(courses)
+
+courses_str = ' - '.join(courses)
+print(courses_str)
+
+
+# courses.sort()
+# courses.append('Java')
+# courses.reverse()
+# print(courses)
+# print(nums)
+# print(len(courses))
