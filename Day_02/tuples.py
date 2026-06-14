d08c4957a8c70a1f305e8e6674d939c26577d6b9
Prakriti Khanal <prakritykhanal1@gmail.com>
2026-06-14 21:12:44 +0545

    Learned tuples

---
+++ Day_02/tuples.py
@@ -0,0 +1,23 @@
+
+# Tuples are immutable
+# tuples_1 = ['maths','english','nepali','science']
+# tuples_2 = tuples_1
+
+# print(tuples_1)
+# print(tuples_2)
+
+# tuples_1[0]='Art'
+# print(tuples_1)
+# print(tuples_2)
+# this will give error since tuples are immutable 
+
+#let's look the same example for the list now 
+lists_1 = ['maths','english','nepali','science']
+lists_2 = lists_1
+
+print(lists_1)
+print(lists_2)
+
+lists_1[0]='Art'
+print(lists_1)
+print(lists_2)
