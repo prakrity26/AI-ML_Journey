b448ee51bcee20264c48267cfb2c65fc638b2563
Prakriti Khanal <prakritykhanal1@gmail.com>
2026-06-14 21:12:13 +0545

    Learned sets

---
+++ Day_02/sets.py
@@ -0,0 +1,17 @@
+cs_course = {'maths','english','nepali','science'}
+# print(cs_course)
+
+art_course = {'maths','english','art','design'}
+
+# print(cs_course.intersection(art_course))
+print(cs_course.union(art_course))
+
+
+
+
+# for creating empty lists, tuples, sets 
+empty_lists = []
+empty_lists = list()
+
+empty_tuples = []
+empty_tuples = tuple()
